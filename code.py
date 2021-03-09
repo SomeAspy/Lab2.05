0ca437568f91b6420c15dec8a54f1e6be8abd9a7
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-03-09 13:52:17 -0500

    test
    Signed-off-by: Aiden <33640860+SomeAspy@users.noreply.github.com>

--- code.py
+++ code.py
@@ -1,23 +1,25 @@
 #Aiden B.
 #Lab2.05
 #3/9/2021
-
-# info:
+#
+#   info:
 #   underline in unicode \u0332
-
-#BOARD
+#   THIS USES EXEC AND IS THEREFORE BAD PRACTICE!!
+#   convert to dictionary when I'm actually good maybe...
+#
+#   BOARD
 #
 #   A1|A2|A3
 #   B1|B2|B3
 #   C1|C2|C3
-
-#init
+#
+#   init
 boardFormat=""
 a1=a2=a3=b1=b2=b3=c1=c2=c3=" "
 LastSet=True
+board="\n"+a1+"|"+a2+"|"+a3+"\n"+b1+"|"+b2+"|"+b3+"e\n"+c1+"|"+c2+"|"+c3
 
 def refresh():
-    board="\n"+a1+"|"+a2+"|"+a3+"\n"+b1+"|"+b2+"|"+b3+"e\n"+c1+"|"+c2+"|"+c3
     global LastSet,boardFormat
     for i in board:
         if i=="e":LastSet=False
@@ -26,3 +28,8 @@ def refresh():
     print(boardFormat)
 
 refresh()
+print("How To Play:\nSpots on the board are named A1-C3, with A being the top row and C being the bottom.")
+
+#SelectedSpace=input("Please choose an area to claim\f").lower()
+
+#exec(SelectedSpace+'="X"')

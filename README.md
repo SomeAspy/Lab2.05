3559bd90daacb4ab46ec9b20fb86ef9870b918d2
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-03-09 12:41:37 -0500

    board functions
    Signed-off-by: Aiden <33640860+SomeAspy@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Lab2.05
 TicTac-toe
+[![Run on Repl.it](https://repl.it/badge/github/SomeAspy/Lab2.05)](https://repl.it/github/SomeAspy/Lab2.05)
---
+++ code.py
@@ -0,0 +1,28 @@
+#Aiden B.
+#Lab2.05
+#3/9/2021
+
+# info:
+#   underline in unicode \u0332
+
+#BOARD
+#
+#   A1|A2|A3
+#   B1|B2|B3
+#   C1|C2|C3
+
+#init
+boardFormat=""
+a1=a2=a3=b1=b2=b3=c1=c2=c3=" "
+LastSet=True
+
+def refresh():
+    board="\n"+a1+"|"+a2+"|"+a3+"\n"+b1+"|"+b2+"|"+b3+"e\n"+c1+"|"+c2+"|"+c3
+    global LastSet,boardFormat
+    for i in board:
+        if i=="e":LastSet=False
+        elif LastSet:boardFormat+=i+"\u0332"
+        elif LastSet==False:boardFormat+=i
+    print(boardFormat)
+
+refresh()

ad27f423872ee77d8f8c018e007bc9564293139f
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-03-12 10:04:12 -0500

    some comments

--- code.py
+++ code.py
@@ -4,28 +4,35 @@
 #
 #   info:
 #   underline in unicode \u0332
-#   Apparently you can't use exec() inside a function, so like, wtf?
 #
 #   BOARD
 #   A1|A2|A3
 #   B1|B2|B3
 #   C1|C2|C3
-#
-#   init
+
+# Import the RegEx library
 import re
+
+# Blank out variables and define them
 a1=a2=a3=b1=b2=b3=c1=c2=c3=" "
 claimed=[""]
+
+# Build and display the board
 def refresh(LastSet=True,BoardFormat=""):
     board="\n "+a1+"|"+a2+"|"+a3+"\n "+b1+"|"+b2+"|"+b3+"\ne "+c1+"|"+c2+"|"+c3
+    # Add underline to all characters until we hit "e"
     for i in board:
-        if i=="e":LastSet=False
-        elif LastSet:BoardFormat+=i+"\u0332"
-        elif LastSet==False:BoardFormat+=i
+        if i=="e":
+            LastSet=False
+        elif LastSet:
+            BoardFormat+=i+"\u0332"
+        elif LastSet==False:
+            BoardFormat+=i
     print(BoardFormat)
 
-print("How To Play:\nSpots on the board are named A1-C3, with A being the top row and C being the bottom.")
-
+# Change position given to X
 def Place(pos):
+    # Reference variables outside the function
     global a1,a2,a3,b1,b2,b3,c1,c2,c3
     if pos=="a1":
         a1="X"
@@ -46,23 +53,28 @@ def Place(pos):
     elif pos=="c3":
         c3="X"
 
+# Check if the space given is claimed
 def IsOpen(space):
     if space in claimed:
         return False
     else:
         return True
 
+# Player's Turn
 def PTurn():
     toCheck=input("Please choose an area to claim\f").lower()
+    # check if input matches RegEx
     if bool(re.match(r"[a-c][1-3]", toCheck)):
         if IsOpen(toCheck):
             global SelectedSpace
             SelectedSpace=toCheck
             Place(SelectedSpace)
+            # Add the given space to the list of claimed spaces
             claimed.append(SelectedSpace)
             refresh()
         else:
             print("That space seems to be occupied! Please input an open space!")
+            # Retry
             PTurn()
     else:
         print("That isn't a valid space, please double check your input!")
@@ -71,7 +83,6 @@ def PTurn():
 
 
 def CPlace(Z,CA1,CA2,CA3,CB1,CB2,CB3,CC1,CC2,CC3):
-    #I'm not making another wtf variable.
     if Z=="a1":
         return CA1
     elif Z=="a2":
@@ -86,6 +97,8 @@ def CPlace(Z,CA1,CA2,CA3,CB1,CB2,CB3,CC1,CC2,CC3):
         return CB3
     elif Z=="c1":
         return CC1
+
+print("How To Play:\nSpots on the board are named A1-C3, with A being the top row and C being the bottom.")
 
 PTurn()
 PTurn()
